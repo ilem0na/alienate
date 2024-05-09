bc0333e2d787fe737e2fe64de45917c12654e01d
Ilemona <ilemona02+git@gmail.com>
2024-05-09 10:11:18 -0600

    alien init settings

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    # Initialize pygame, settings and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+    
+    # start main loop for game 
+    while True:
+        
+        # watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        
+run_game()
+         
+        
+    
